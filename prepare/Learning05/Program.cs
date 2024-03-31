f3d15b83d89228ed0af2b3baceb6e6c8cec69b0e
ToKolob <anolis27@hotmail.com>
2024-03-31 16:47:48 -0300

    ok

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("red", 4));
+        shapes.Add(new Rectangle("red", 4, 5));
+        shapes.Add(new Circle("red", 4));
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea()}.");
+        }
+
+    }
+}
+public class Shape
+{
+    private string _color;
+    public Shape(string color)
+    {
+        _color = color;
+    }
+    public string GetColor()
+    {
+        return _color;
+    }
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
+public class Square : Shape
+{
+    private double _side;
+    public Square(string color, double side) : base(color)
+    {
+        _side = side;
+    }
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}
+public class Rectangle : Shape
+{
+    private double _length;
+    private double _width;
+    public Rectangle(string color, double length, double width) : base(color)
+    {
+        _length = length;
+        _width = width;
+    }
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+}
+public class Circle : Shape
+{
+    private double _radius;
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+    public override double GetArea()
+    {
+        return Math.PI * _radius * _radius;
     }
 }
